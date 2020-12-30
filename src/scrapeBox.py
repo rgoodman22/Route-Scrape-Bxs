f153b452de245d49b72f974b98e5f0ca6ec56455
rgoodman22 <robertgoodman2022@u.northwestern.edu>
2020-12-29 19:44:10 -0500

    scrapeBox details

--- src/scrapeBox.py
+++ src/scrapeBox.py
@@ -1,15 +1,46 @@
 from selenium.webdriver.common.keys import Keys
 from chromeSync import driver
+import time
 #webdriverwait related imports
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+monthDict = {
+    "JANUARY" : "1",
+    "FEBRUARY" : "2",
+    "MARCH" : "3",
+    "APRIL" : "4",
+    "MAY" : "5",
+    "JUNE" : "6",
+    "JULY" : "7",
+    "AUGUST" : "8",
+    "SEPTEMBER" : "9",
+    "OCTOBER" : "10",
+    "NOVEMBER" : "11",
+    "DECEMBER" : "12"
+}
+
+def parseDate(date):
+    date = date.split(' ')[:3]
+    month = monthDict[date[0]]
+    day = date[1][:2]
+    year = date[2]
+
+    return month + "/" + day + "/" + year
+
 class scrapeBox:
     def __init__(self, url, driver):
+        self.data = {}
         self.driver = driver
         self.url = url
         self.moveToBox()
+        self.getTeams()
+        self.getDetails()
+        self.parseBox(0)
+        self.switchTeam()
+        self.parseBox(1)
+        
 
     def moveToBox(self):
         if 'boxscore' in self.url:
@@ -18,19 +49,49 @@ class scrapeBox:
             self.url += 'boxscore'
         else:
             self.url += '/boxscore'
-        self.driver.get(self.url)
-        source = self.driver.page_source
-    
+        self.driver.get(self.url)    
     
     def switchTeam(self):
-        pass
+        time.sleep(.5)
+        myXpath = "//div[@class='boxscore-team-selector-team homeTeam-bg-primary_color awayTeam-border-primary_color home']"
+        homeTeam = self.driver.find_element_by_xpath(myXpath)
+        driver.execute_script("arguments[0].click();", homeTeam)
+    
+    
+    def parseBox(self, team):
+        if team == 0:
+            cur, opp = self.teams
+        else:
+            opp, cur = self.teams
+        myXpath = "//div[@class='boxscore-table-collection']"
+        tables = self.driver.find_element_by_xpath(myXpath)
+        tables = tables.find_elements_by_xpath("//table")
 
+        for table in tables:
+            table = table.find_elements_by_class_name("tableHeadCollegeName")
+            print('hi')
+            for t in table:
+                print(t.get_attribute("innerHTML"))
+    
+    def getTeams(self):
+        self.teams = []
+        myXpath = "//div[@class='boxscore-team-selector-team awayTeam-bg-primary_color homeTeam-border-primary_color away active']"
+        away = self.driver.find_element_by_xpath(myXpath)
+        self.teams.append(away.text)
+
+        myXpath = "//div[@class='boxscore-team-selector-team homeTeam-bg-primary_color awayTeam-border-primary_color home']"
+        home = self.driver.find_element_by_xpath(myXpath)
+        self.teams.append(home.text)
+
+    def getDetails(self):
+        myXpath = "//span[@class='venue']"
+        self.date = self.driver.find_element_by_xpath(myXpath).text
+        self.date = parseDate(self.date)
+    
 
 url = 'https://www.ncaa.com/game/3959666/boxscore'
-#GET TEAM
-#GET OPPONENT
-#GET DATE
-#GET LOCATION
+#url = 'https://www.ncaa.com/game/football/d3/2013/09/07/alma-heidelberg/'
+
 scrapeBox(url, driver)
 
         
